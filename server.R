492d267ce6b2a97fd1aae9f59c9e7d8c939557a0
mac <mac@macs-MBP.cable.rcn.com>
2022-03-17 16:36:09 -0400

    basic set up

--- server.R
+++ server.R
@@ -1,38 +1,55 @@
 library(shiny)
+library(shinyjs)
+library(async)
+library(promises)
+library(future)
+library(shinyWidgets)
 
+source("./servo_functions.R")
 
-startServo <- function() {
-  response = httr::GET(
-    paste0(
-      Sys.getenv("BASE_URL"),
-      "start"
+killAll <- function() {
+  
+  url <- paste0(
+    Sys.getenv("BASE_URL"),
+    "kill_all"
+  )
+  tryCatch(
+    expr = {
+      resp <- httr::GET(url)
+      resp <- jsonlite::fromJSON(rawToChar(resp$content))
+      
+    }, error = function(e) {
+      list(connection_status = "connection error")
+    }, warning = function(w) {
+      list(connection_status = "connection error")
+    }
+  )
+}
+
+# test connection function
+testConnection <- function() {
+  
+  tryCatch(
+    expr = {
+      resp <- httr::GET(
+        paste0(
+          Sys.getenv("BASE_URL"),
+          "amiconnected"
+        )
       )
-    )
-  print(response)
-}
-
-stopServo <- function() {
-  response = httr::GET(
-    paste0(
-      Sys.getenv("BASE_URL"),
-      "stop"
-    )
+      
+      resp <- jsonlite::fromJSON(rawToChar(resp$content))
+    },
+    error = function(e){ 
+      list(connection_status = "connection error")
+    },
+    warning = function(w){
+      list(connection_status = "connection warning")
+    }
   )
-  print(response)
 }
 
-testServo <- function() {
-  response = httr::GET(
-    paste0(
-      Sys.getenv("BASE_URL"),
-      "test"
-    )
-  )
-  print(response)
-}
-
-
-server <- function(input, output) {
+server <- function(input, output, session) {
   
   output$ui <- renderUI({
     uiOutput("authenticated") 
@@ -42,43 +59,177 @@ server <- function(input, output) {
   output$authenticated <- renderUI({
     
     fluidPage(
-      fluidRow(
-        mainPanel(
-          tabsetPanel(
-            tabPanel("Log Blood Pressure", uiOutput("dash"))
-          )
-        )
-      )
+      setBackgroundColor(
+        color = c("#89CFF0")
+      ),
+      useShinyjs(),
+      br(),
+      uiOutput("connection_panel"),
+      uiOutput("main")
     )
   })
   
-  output$dash <- renderUI({
+  output$connection_panel <- renderUI({
     fluidPage(
-      fluidRow(
-        actionButton("start", "Start Servo")
-      ),
-      br(),
-      fluidRow(
-        actionButton("stop", "Stop Servo")
-      ),
-      br(),
-      fluidRow(
-        actionButton("test", "Test Servo")
+      column(12, align="right",
+             fluidRow(
+               htmlOutput("connection_status")
+             ),
+             fluidRow(
+               textOutput("job_status")
+             )
+        )
+    )
+  })
+  
+  output$main <- renderUI({
+    fluidRow(
+      column(12,align="center",
+             br(),
+             fluidRow(
+               
+               actionButton("amiconnected", "Am I Connected?")
+             ),
+             br(),
+     
+               fluidRow(
+               #actionButton("killall", "Stop the Magic")
+               tags$button(
+                 id = "killall",
+                 class = "btn action-button",
+                 tags$img(src = "stop.png",
+                          height = "100px")
+               ),
+               fluidRow(
+                 p("Stop the Magic")
+               )
+               
+             ),
+             br(),
+             fluidRow(
+               #actionButton("beginner", "My Cat Likes Gentle Play"),
+               tags$button(
+                 id = "beginner",
+                 class = "btn action-button",
+                 tags$img(src = "catBeginner.png",
+                          height = "100px")
+               ),
+               fluidRow(
+                 p("My Cat Likes Gentle Play")
+               )
+             ),
+             br(),
+             fluidRow(
+               #actionButton("intermediate", "My Cat likes Adventure"),
+               tags$button(
+                 id = "intermediate",
+                 class = "btn action-button",
+                 tags$img(src = "catIntermediate.png",
+                          height = "100px")
+               ),
+               fluidRow(
+                 p("My Cat likes Adventure")
+               )
+             ),
+             br(),
+             fluidRow(
+               #actionButton("advanced", "My Cat is a Nut"),
+               tags$button(
+                 id = "advanced",
+                 class = "btn action-button",
+                 tags$img(src = "catIntermediate.png",
+                          height = "100px")
+               ),
+               fluidRow(
+                 p("My Cat is a Nut")
+               )
+             )
       )
     )
   })
   
   
-  observeEvent(input$start, {
-    startServo()
+  # Connection Management -----
+  connection <- reactiveValues(
+    connection_status = NULL,
+    job_type = NULL,
+    job_pid = NULL
+  )
+  
+  buttons <- reactiveValues(
+    beginner = FALSE,
+    intermediate = FALSE,
+    advanced = FALSE
+  )
+  
+  # Event Managment ------------
+  observeEvent(input$beginner, {
+    
+    # disable other buttons
+    shinyjs::disable("intermediate")
+    shinyjs::disable("advanced")
+    
+    # coms
+    buttons$beginner <- TRUE
+    mode <- "beginner"
+    con <- beginnerServo(mode)
+    connection$job_type <- con$job_type
+    job_pid <- con$job_pid
   })
   
-  observeEvent(input$start, {
-    stopServo()
+  observeEvent(input$intermediate, {
+    
+    # disable other buttons
+    shinyjs::disable("beginner")
+    shinyjs::disable("advanced")
+    
+    # coms
+    buttons$intermediate <- TRUE
+    mode <- "intermediate"
+    con <- beginnerServo(mode)
+    connection$job_type <- con$job_type
+    job_pid <- con$job_pid
   })
   
-  observeEvent(input$start, {
-    testServo()
+  observeEvent(input$advanced, {
+    
+    # disable other buttons
+    shinyjs::disable("intermediate")
+    shinyjs::disable("beginner")
+    
+    # coms
+    buttons$advanced <- TRUE
+    mode <- "advanced"
+    con <- beginnerServo(mode)
+    connection$job_type <- con$job_type
+    job_pid <- con$job_pid
   })
   
+  observeEvent(input$killall, {
+    con <- killAll()
+    connection$job_type <- 'Job Stopped'
+    job_pid <- NULL
+    
+    # enable any disabled buttons
+    shinyjs::enable("beginner")
+    shinyjs::enable("advanced")
+    shinyjs::enable("intermediate")
+  })
+  
+  observeEvent(input$amiconnected, {
+    #print("testing connection")
+    con <- testConnection()
+    connection$connection_status <- con$connection_status
+    
+  })
+  
+    output$connection_status <- renderText({
+      paste0("Connection Status: ",if(is.null(connection$connection_status)) { "Not Connected" } else { paste("<b>",isolate(connection$connection_status),"</b>") } )
+    })
+    
+    output$job_status <- renderText({
+      paste0("Job Status: ",if(is.null(connection$job_type)) { "No Job Running" } else { isolate(connection$job_type) } )
+      
+    })
+   
 }
